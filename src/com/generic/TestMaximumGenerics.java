ee3b68a75aa2d3d802b177f88992f201cf6af58a
Atulsak <sakhareat@gmail.com>
2022-12-09 12:58:52 +0530

    ADDED: test case 1.2 maximum integer at third postion

--- src/com/generic/TestMaximumGenerics.java
+++ src/com/generic/TestMaximumGenerics.java
@@ -2,7 +2,34 @@ package com.generic;
 
 public class TestMaximumGenerics {
 
-public static void main(String[] args) {
-System.out.println("Welcome to genrics ");
-}
+	public static Integer maximumInteger(Integer first, Integer second, Integer third) {
+		if (first > second && first > third) {
+			return first;
+		} else if (second > third) {
+			return second;
+		} else {
+			return third;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to test maximum generics program");
+		System.out.println("Test case 1.1");
+		// Test Case 1.1 maximum number at first position 
+		System.out.println("Maximum of 3, 2, 1 is : "+TestMaximumGenerics.maximumInteger(3, 2, 1));
+		System.out.println("Maximum of 3, 1, 2 is : "+TestMaximumGenerics.maximumInteger(3, 1, 2));
+		System.out.println("Maximum of 3, 1, 1 is : "+TestMaximumGenerics.maximumInteger(3, 1, 1));
+		
+		System.out.println("Test case 1.2");
+		// Test Case 1.1 maximum number at first position 
+		System.out.println("Maximum of 2, 3, 1 is : "+TestMaximumGenerics.maximumInteger(2, 3, 1));
+		System.out.println("Maximum of 1, 3, 2 is : "+TestMaximumGenerics.maximumInteger(1, 3, 2));
+		System.out.println("Maximum of 1, 3, 1 is : "+TestMaximumGenerics.maximumInteger(1, 3, 1));
+		
+		System.out.println("Test case 1.3");
+		// Test Case 1.1 maximum number at first position 
+		System.out.println("Maximum of 2, 1, 3 is : "+TestMaximumGenerics.maximumInteger(2, 1, 3));
+		System.out.println("Maximum of 1, 2, 3 is : "+TestMaximumGenerics.maximumInteger(1, 2, 3));
+		System.out.println("Maximum of 1, 1, 3 is : "+TestMaximumGenerics.maximumInteger(1, 1, 3));
+	}
 }
